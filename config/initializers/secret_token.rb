3f13a876457a62cfcb1fafff59698eddb26da435
JoseJRVazquez <kwikteck@gmail.com>
2014-05-02 08:17:01 -0500

    Initial commit
    Initial commit without standard testing included in favor of using
    RSpec later.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dropcloth::Application.config.secret_key_base = '94d03eef8488807c411b0c2e13993392913f81cd4a2e84248e353875873f933aa8b4b6607064fd21105b04c50c26267cbf5adebe38985ce18f52db0828e396ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dropcloth::Application.config.session_store :cookie_store, key: '_dropcloth_session'
